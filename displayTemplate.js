b68ae28ff01cc565e775cd53a87f682ab747a6d7
MasamiYamate <MasamiYamate@MasamiYamate-MacBookAir.local>
2018-12-05 16:46:45 +0900

    Add to ListTemplateMethod

--- displayTemplate.js
+++ displayTemplate.js
@@ -51,6 +51,55 @@ function createBodyTemplate (typeName , setData) {
     return responseData
 }
 
+function createListTemplate (typeName , setData) {
+    var responseData = {type:typeName}
+    //Tokenの付与
+    if (setData['token']) {
+        responseData['token'] = setData['token']
+    }else{
+        responseData['token'] = 'unknownToken'
+    }
+    //BackButtonの表示非表示
+    if (setData['backButton']) {
+        responseData['backButton'] = setData['backButton']
+    }else{
+        responseData['backButton'] = 'VISIBLE'
+    }
+    //Titleの付与
+    if (setData['title']) {
+        responseData['title'] = setData['title']
+    }else{
+        responseData['title'] = ''
+    }
+    //背景画像の付与
+    if (setData['backgroundImage']) {
+        responseData['backgroundImage'] = createImageContent(setData['backgroundImage']) 
+    }
+    //リストアイテムの付与
+    var setItems = []
+    if (setData['listItems']) {
+        setItems = createListItems(setData['listItems'])
+    }
+    responseData['listItems'] = setItems
+    return responseData
+}
+
+function createListItems (items) {
+    var responseData = []
+    for (var i in items) {
+        let item = items[i]
+        if (item['token'] && item['textContent']) {
+            var setItem = {'token':item['token']}
+            if (item['src']) {
+                setItem['image'] = createImageContent(item['src'])
+            }
+            setItem['textContent'] = createTextContent(item['textContent'])
+            responseData.push(setItem)
+        }
+    }
+    return responseData
+}
+
 function createImageContent(src) {
     return new Alexa.ImageHelper()
         .addImageInstance(src)
