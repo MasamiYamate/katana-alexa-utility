02398e048f6c96cb2b3b594433d75457e6f1ca3a
MasamiYamate <MasamiYamate@MasamiYamate-MacBookAir.local>
2018-12-06 13:59:47 +0900

    Add SyncAPIRequest.js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const device            = require('./device')
 const template          = require('./displayTemplate')
 const dynamodb          = require('./dynamodb')
 const slot              = require('./slot')
+const syncAPIRequest    = require('./syncRequest') 
 
 module.exports = {
     device: device ,
     template: template,
     dynamodb: dynamodb,
-    slot: slot
+    slot: slot,
+    syncAPIRequest: syncAPIRequest
 }
---
+++ syncRequest.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+/*http同期処理を行う*/
+exports = async function (url) {
+    let response = function () {
+        return new Promise(function (resolve , reject){
+            request.get({
+                url:url
+            } , function (error , response , body) {
+                if(!error) {
+                    resolve(body);
+                } else {
+                    reject(error);
+                }
+            })
+        });
+    }
+    let res = await response();
+    var jsonData = JSON.parse(res);
+    return jsonData;
+}
