057398b1c672034496e0b3437bed42da4541d2a6
MasamiYamate <MasamiYamate@MasamiYamate-MacBookAir.local>
2018-12-06 10:14:44 +0900

    create bodyTemokate method

--- displayTemplate.js
+++ displayTemplate.js
@@ -1,14 +1,76 @@
 const Alexa = require('ask-sdk-core')
 
-exports.addBodyTemplate = function (handlerInput , templateType , setData) {
-    if (handlerInput && templateType && setData) {
-        let addTemplate = createBodyTemplate(templateType , setData)
+module.exports.bodyType = {
+    //Tokenをセットします
+    setToken: function (token) {
+        this.token = token
+    },
+    //戻るボタン
+    setBackBtnIsVisible: function (isVisible) {
+        this.backButton = isVisible
+    },
+    //Titleの設定
+    setTitle: function (title) {
+        this.titleTxt = title
+    },
+    //背景画像パス、のセット
+    setBackgroundImgSrc: function (src) {
+        this.backgroundImageSrc = src
+    },
+    //テンプレート内で表示する画像パスのセット
+    setImgSec: function (src) {
+        this.imageSrc = src
+    },
+
+    //テンプレート内で表示する文字列をセット
+    setText: function (primaryText , secondaryText , tertiaryText) {
+        this.primaryText    = primaryText
+        this.secondaryText  = secondaryText
+        this.tertiaryText   = tertiaryText
+    },
+
+    //ヒント文をセットします ※Bodytemplate3のみ有効
+    setHintMsg: function (msg) {
+        this.hint = msg
+    },
+
+    //テンプレートを生成する
+    create: function (handlerInput , typeName) {
+        var setData = {}
+        if (this.token) {
+            setData['token'] = this.token
+        }
+        if (this.isVisible) {
+            setData['backButton'] = this.isVisible
+        }
+        if (this.titleTxt) {
+            setData['title'] = this.titleTxt
+        }
+        if (this.backgroundImageSrc) {
+            setData['backgroundImage'] = this.backgroundImageSrc
+        }
+        if (this.imageSrc) {
+            setData['image'] = this.imageSrc
+        }
+        var setTextData = {'primaryText': '' , 'secondaryText': '', 'tertiaryText':''}
+        if (this.primaryText) {
+            setTextData['primaryText'] = this.primaryText
+        }
+        if (this.secondaryText) {
+            setTextData['secondaryText'] = this.secondaryText
+        }
+        if (this.tertiaryText) {
+            setTextData['tertiaryText'] = this.tertiaryText
+        }
+        setData['textContent'] = setTextData
+
+        let addTemplate = createBodyTemplate(typeName , setData)
         //付与対象のテンプレートがある場合は、付与します
         if (addTemplate) {
             handlerInput.responseBuilder.addRenderTemplateDirective(addTemplate)
-        }
-        if (templateType == 'BodyTemplate3' && setData[hintMsg]) {
-            handlerInput.responseBuilder.addHintDirective(setData[hintMsg])
+            if (typeName == 'BodyTemplate3' && this.hint) {
+                handlerInput.responseBuilder.addHintDirective(this.hint)
+            }
         }
     }
 }
--- index.js
+++ index.js
@@ -1,40 +1,11 @@
 const device            = require('./device')
-const displayTemplate   = require('./displayTemplate')
+const template          = require('./displayTemplate')
 const dynamodb          = require('./dynamodb')
 const slot              = require('./slot')
 
 module.exports = {
-    isDisplay: function (handlerInput) {
-        return device.isDisplay(handlerInput)
-    },
-    getScreenType: function (handlerInput) {
-        return device.getScreenType(handlerInput)
-    },
-    getScreenSize: function (handlerInput) {
-        return device.getScreenSize(handlerInput)
-    },
-    getApplicationId: function (handlerInput) {
-        return device.getApplicationId(handlerInput)
-    },
-    getUserId: function (handlerInput) {
-        return device.getUserId(handlerInput)
-    },
-    getDeviceId: function (handlerInput) {
-        return device.getDeviceId(handlerInput)
-    },
-
-    addBodyTemplate: function (handlerInput , templateType , data) {
-        return displayTemplate.addBodyTemplate(handlerInput , templateType , data)
-    },
-
-    saveDynamoDBData: async function (handlerInput , data) {
-        await dynamodb.saveData(handlerInput , data)
-    },
-    getDynamoDBData: async function (handlerInput) {
-        return await dynamodb.getData(handlerInput)
-    },
-
-    getSlotValue: function (handlerInput , slotName , isDebug) {
-        return slot.getSlotValue(handlerInput , slotName , isDebug)
-    }
+    device: device ,
+    template: template,
+    dynamodb: dynamodb,
+    slot: slot
 }
